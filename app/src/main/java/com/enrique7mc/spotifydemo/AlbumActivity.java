525d55ec93c318f8d7d29e387bbd6fef6c343c78
enrique7mc <enrique7mc@gmail.com>
2016-05-08 19:17:25 -0500

    Refactor imports

--- app/src/main/java/com/enrique7mc/spotifydemo/AlbumActivity.java
+++ app/src/main/java/com/enrique7mc/spotifydemo/AlbumActivity.java
@@ -1,8 +1,7 @@
 package com.enrique7mc.spotifydemo;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
